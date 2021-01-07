dd95964aeadbc5ba10417ad95a28f6bf4b36a772
nclslbrn <nl@artemg.com>
2021-01-07 20:30:30 +0100

    replace apache2 with  nginx

--- webserver.sh
+++ webserver.sh
@@ -4,23 +4,23 @@
 #
 # Syntax: sudo ./webServer.sh [start|restart|stop]
 #
-# Boubakr NOUR <n.boubakr@gmail.com>
+# Original from Boubakr NOUR <n.boubakr@gmail.com>
 # Distributed under the GPL version 3 license
 
 case "$1" in
 	"start")
-		#echo "Starting Web Server"
-		sudo apachectl start && sudo /usr/local/mysql/support-files/mysql.server start && open -a "Google Chrome" 'http://localhost/'
+		echo "Starting Web Server"
+		sudo systemctl start nginx && sudo systemctl start mariadb && sudo systemctl start php-fpm
 		;;
 	"restart")
-		#echo "Restarting Web Server"
-		sudo apachectl restart && sudo /usr/local/mysql/support-files/mysql.server restart
+		echo "Restarting Web Server"
+		sudo systemctl restart nginx && sudo systemctl restart mariadb && sudo systemctl restart php-fpm
 		;;
 	"stop")
-		#echo "Stopping Web Server"
-		sudo apachectl stop && sudo /usr/local/mysql/support-files/mysql.server stop
+		echo "Stopping Web Server"
+		sudo systemctl stop nginx && sudo systemctl stop mariadb && sudo systemctl stop php-fpm
 		;;
 	* )
-		#echo "Usage: $0 [start|restart|stop]"
+		echo "Usage: $0 [start|restart|stop]"
 	;;
 esac
